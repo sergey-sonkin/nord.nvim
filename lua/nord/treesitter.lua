3e096150c3d15c8e64f702ec988ed0ae0c8fab41
Sergey Sonkin <mastersmail1@gmail.com>
2024-07-20 20:20:06 -0400

    Made builtin constants purple

--- lua/nord/treesitter.lua
+++ lua/nord/treesitter.lua
@@ -89,7 +89,8 @@ function treesitter.highlights()
     ["@variable.builtin"] = { fg = c.frost.artic_water }, --  built-in variable names (e.g. `this`)
     ["@variable.builtin.python"] = { fg = c.frost.origin }, --  "self" will render as variable
     ["@constant"] = { fg = c.snow_storm.origin }, --  constant identifiers
-    ["@constant.builtin"] = { fg = c.frost.artic_water },
+    -- ["@constant.builtin"] = { fg = c.frost.artic_water },
+    ["@constant.builtin"] = { fg = c.aurora.purple },
     ["@constant.macro"] = { fg = c.frost.artic_water }, --  constants defined by the preprocessor
     ["@module"] = { fg = c.snow_storm.origin }, --  modules or namespaces
     ["@namespace"] = { link = "@module" }, -- @deprecated
